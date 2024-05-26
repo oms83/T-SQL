ca72fa11f8c542f3489fd95ef4c0c7a7851aa7bd
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-26 09:09:50 +0300

    Update CaseWithGroupBy.cs

--- CaseWithGroupBy.cs
+++ CaseWithGroupBy.cs
@@ -138,3 +138,36 @@ public class CaseWithGruopBy
     }
 
 }
+
+/*
+---------------------------------------------------------------------------------------
+Use C21_DB1
+
+SELECT PerformanceCategory, COUNT(*) AS NumberOfEmployees, AVG(Salary) AS AverageSalary FROM
+(
+	SELECT Name, Salary, CASE 
+							WHEN PerformanceRating >= 80 THEN 'High'
+							WHEN PerformanceRating >= 60 THEN 'Medium'
+							ELSE 'Low'
+						 END AS PerformanceCategory
+	FROM Employees2
+) AS PerformanceTable
+GROUP BY PerformanceCategory
+---------------------------------------------------------------------------------------
+SELECT EmployeeID, Count(*) As PersentDays FROM
+(
+	SELECT EmployeeID FROM EmployeeAttendance WHERE Status ='Present'
+) AS t
+GROUP BY EmployeeID
+---------------------------------------------------------------------------------------
+SELECT EmployeeID, 
+COUNT(CASE WHEN Status = 'Present' THEN 1 END) As PersentDays 
+FROM EmployeeAttendance
+GROUP BY EmployeeID
+---------------------------------------------------------------------------------------
+SELECT EmployeeID, 
+SUM(CASE WHEN Status = 'Present' THEN 1 ELSE 0 END) As PersentDays 
+FROM EmployeeAttendance
+GROUP BY EmployeeID
+---------------------------------------------------------------------------------------
+*/
