f887643133eab2955c76b65ad0bae12474ef0ac6
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-26 11:15:56 +0300

    Create WhileNestedMultiTable.sql

---
+++ WhileNestedMultiTable.sql
@@ -0,0 +1,17 @@
+USE C21_DB1
+
+DECLARE @x INT = 1;
+DECLARE @y INT = 1;
+
+WHILE @x <= 10
+BEGIN 
+	SET @y = 0;
+	WHILE @y <= 10
+	BEGIN
+		PRINT CAST(@x AS VARCHAR) + '*' + CAST(@y AS VARCHAR) + '=' + CAST(@x*@y AS VARCHAR);
+		SET @y = @y + 1;
+	END
+	PRINT '';
+	SET @x = @x + 1;
+END
+
