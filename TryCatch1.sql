de4bfb1c7d71a73f7018ab3480130bde9bc99f5f
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-27 09:20:10 +0300

    Create TryCatchExample1.sql

---
+++ TryCatch1.sql
@@ -0,0 +1,21 @@
+USE C21_DB1 
+
+GO
+CREATE TABLE Employees3
+(
+	EmployeeID INT PRIMARY KEY,
+	Name VARCHAR(100),
+	Position VARCHAR(100)
+);
+
+GO
+BEGIN TRY
+	INSERT INTO Employees3 (EmployeeID, Name, Position) VALUES (1, 'Omer MEMES', 'IT');
+	INSERT INTO Employees3 (EmployeeID, Name, Position) VALUES (1, 'Al MEMES', 'IT');
+END TRY
+
+BEGIN CATCH
+	PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
+
+-- An error occurred: Violation of PRIMARY KEY constraint 'PK__Employee__7AD04FF17A3550C3'. Cannot insert duplicate key in object 'dbo.Employees3'. The duplicate key value is (1).
