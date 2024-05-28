f2a1e7d35bf61ed6e08c6d75ce0dfd843a0b774c
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-28 22:23:02 +0300

    Update AggregateFunctions.sql

--- AggregateFunctions.sql
+++ AggregateFunctions.sql
@@ -7,3 +7,16 @@ SELECT COUNT(1) AS Employees FROM Employees2 GROUP BY Department;
 SELECT MAX(Salary) FROM Employees2 GROUP BY Department;
 
 SELECT MIN(Salary) FROM Employees2 GROUP BY Department;
+
+
+SELECT e1.Name, e1.Department, T.MaxSalary
+FROM 
+(
+    SELECT Department, MAX(Salary) AS MaxSalary
+    FROM Employees2
+    GROUP BY Department
+) T
+INNER JOIN Employees2 e1 ON e1.Department = T.Department AND e1.Salary = T.MaxSalary
+ORDER BY e1.Department;
+
+استعلام بيعطي نتيجة أعلى راتب موظف في كل قسم
