e64fd2a560e6fe500814807ab85f7e1eb054d6d9
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-29 20:17:29 +0300

    Create TableValuedMultipleStatement.sql

---
+++ TableValuedMultipleStatement.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION GetPerformingStudents()
+RETURNS @Result TABLE
+(
+	SID INT, 
+	SName VARCHAR(100),
+	SSubject VARCHAR(100),
+	SGrade INT
+)
+AS 
+BEGIN
+	INSERT INTO @Result (SID, SName, SSubject, SGrade)
+	SELECT TOP 3 StudentID, Name, Subject, Grade 
+	FROM Students
+	ORDER BY Grade DESC;
+
+	RETURN;
+END
+
+
+SELECT * From GetPerformingStudents()
