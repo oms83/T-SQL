51c6c7483d3a9f5c303a0687aa13a27062b53926
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-29 16:41:50 +0300

    Update ScalarFunctions.sql

--- ScalarFunctions.sql
+++ ScalarFunctions.sql
@@ -13,3 +13,27 @@ INSERT INTO Teachers (TeacherID, Name, Subject) VALUES
 (4, 'Mark Brown', 'History'),
 (5, 'Sarah Davis', 'Music');
 ------
+-- When create a function query you should not write anything(query, command) above the otherwise else you will take an error
+CREATE FUNCTION GetGradeAverage(@subject VARCHAR(100))
+RETURNS INT
+AS
+BEGIN
+	DECLARE @gradeAverage INT;
+	SELECT @gradeAverage = AVG(Grade) FROM Students
+	WHERE Subject = @subject;
+	Return @gradeAverage;
+END
+--
+
+SELECT Name, dbo.GetGradeAverage(Subject) FROM Students
+/*
+Alice	84
+Bob	84
+Charlie	84
+Dave	88
+Emma	88
+Fiona	88
+Grace	81
+Henry	81
+Isabella	81
+*/
