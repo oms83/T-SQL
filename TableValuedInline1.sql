169a4f39a37706bb149efa4a64a207ab3ed76b26
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-29 17:53:36 +0300

    Update TableValuedInline1.sql

--- TableValuedInline1.sql
+++ TableValuedInline1.sql
@@ -1,5 +1,11 @@
 -- INLINE TABLE VALUED FUNCTION
 -- USES ONLY ONE SELECT STATEMENT
+
+-- You cannot use UPDATE or INSERT statements in Inline Table-Valued Functions (ITVFs) in T-SQL. Inline Table-Valued Functions are designed to be 
+-- read-only, and they return a table variable that is essentially a result of a single SELECT statement. Since these functions are read-only, 
+-- they cannot modify the data in the database, which means you cannot perform data manipulation operations like INSERT, UPDATE, DELETE, or MERGE within them.
+
+
 CREATE FUNCTION GetStudentsBySubject(@subjects VARCHAR(50))
 RETURNS TABLE
 AS
