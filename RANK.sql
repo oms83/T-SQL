1271d1724b40a9dda78eb3472b85e73db63aea89
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-29 05:40:16 +0300

    Create RANK.sql

---
+++ RANK.sql
@@ -0,0 +1,37 @@
+Use C21_DB1
+--
+SELECT * FROM Students ORDER BY Grade DESC;
+
+--
+SELECT *, ROW_NUMBER() OVER (ORDER BY Grade DESC) AS RowNumber 
+FROM Students 
+ORDER BY Grade DESC
+  
+/*
+    ROW_NUMBER
+Science	      92	1
+Math        	90	2
+Science	      88	3
+English	      88	4
+Math	        85	5
+Science	      84	6
+English	      80	7
+Math	        78	8
+*/
+  
+--
+SELECT *, RANK() OVER (ORDER BY Grade DESC) AS RowNumber 
+FROM Students 
+ORDER BY Grade DESC
+
+/*
+        RANK
+Science	      92	1
+Math        	90	2
+Science	      88	3
+English	      88	3
+Math	        85	5
+Science	      84	6
+English	      80	7
+Math	        78	8
+*/
