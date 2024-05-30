a3007112e9a5d1226c77de55da8627442b555ef3
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-30 16:24:16 +0300

    Create Trigger_Instead_Update.sql

---
+++ Trigger_Instead_Update.sql
@@ -0,0 +1,67 @@
+CREATE TABLE StudentInfo
+(
+	StudentID INT IDENTITY PRIMARY KEY,
+	Name VARCHAR(100),
+	Address VARCHAR(500),
+);
+
+CREATE TABLE AcademicInfo
+(
+	StudentID INT,
+	Course VARCHAR(100),
+	Grade INT,
+	FOREIGN KEY (StudentID) REFERENCES StudentInfo(StudentID)
+);
+
+--ALTER TABLE AcademicInfo
+--ALTER COLUMN StudentID INT 
+
+--DROP TABLE AcademicInfo
+--DROP TABLE StudentInfo
+
+INSERT INTO AcademicInfo (StudentID, Course, Grade) VALUES
+(1, 'Computer Science', 90),
+(2, 'Mathematics', 85);
+
+INSERT INTO StudentInfo (Name, Address) VALUES
+('John Doe', '123 Main St'),
+('Jane Smith', '456 Oak Ave');
+------------------------------------------------------------
+
+
+CREATE VIEW StudentView AS
+SELECT P.StudentID, P.Name, P.Address, A.Course, A.Grade
+FROM StudentInfo P
+JOIN AcademicInfo A ON P.StudentID = A.StudentID;
+------------------------------------------------------------
+
+
+CREATE TRIGGER trg_InsteadUpdateStudent ON StudentView
+INSTEAD OF UPDATE
+AS 
+BEGIN 
+		UPDATE StudentInfo 
+		SET Name = I.Name, Address = I.Address 
+		FROM  StudentInfo S
+		INNER JOIN inserted I ON S.StudentID = I.StudentID
+
+		UPDATE AcademicInfo 
+		SET Grade = I.Grade, Course	 = I.Course
+		FROM  AcademicInfo A
+		INNER JOIN inserted I ON A.StudentID = I.StudentID
+
+END
+
+
+SELECT * FROM StudentView;
+
+UPDATE StudentView
+SET Grade = 100, Name = 'Omer MEMES'
+WHERE StudentID = 1
+--(1 row affected)
+--(1 row affected)
+--(1 row affected)
+
+
+
+
