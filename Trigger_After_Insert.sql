316e8912cfbf06a174279bc2bb870a5769bb25a5
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-30 11:21:53 +0300

    Update Trigger_After_Insert.sql

--- Trigger_After_Insert.sql
+++ Trigger_After_Insert.sql
@@ -38,4 +38,12 @@ AFTER INSERT: Indicates that the trigger responds to insert operations.
 
 INSERT INTO StudentInsertLog(...) SELECT ... FROM inserted: The core action, which inserts a record into the 'StudentInsertLog' table. 
 The inserted table is a special table used within the scope of insert and update triggers, holding the newly inserted rows.
+
+What is an After Insert Trigger in T-SQL?
+It is a trigger that is executed automatically after a record is inserted into a table.
+
+What are the benefits of After Insert Triggers?
+Maintaining data integrity and reliability in a database.
+Automating logging of new entries in a database.
+Implementing complex business rules in a database.
 */
