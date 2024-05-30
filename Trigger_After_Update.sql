002b18e7b0ff23bc163d2b0b32cf1cff84226389
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-30 12:55:19 +0300

    Update Trigger_After_Update.sql

--- Trigger_After_Update.sql
+++ Trigger_After_Update.sql
@@ -39,11 +39,27 @@ SELECT * FROM StudentUpdateLog
 -- 1	10	100	98	2024-05-30 12:35:54.243
 
 /*
-  CREATE TRIGGER trg_AfterUpdateStudent: Creates a trigger named trg_AfterUpdateStudent.
-  ON Students: Associates the trigger with the 'Students' table.
-  AFTER UPDATE: The trigger is fired after an update operation.
-  IF UPDATE(Grade): Checks if the 'Grade' column was part of the update.
-  The inserted and deleted tables: Special tables where inserted contains the new values and deleted contains the old values of the updated rows.
+	
+	CREATE TRIGGER trg_AfterUpdateStudent: Creates a trigger named trg_AfterUpdateStudent.
+	ON Students: Associates the trigger with the 'Students' table.
+	AFTER UPDATE: The trigger is fired after an update operation.
+	IF UPDATE(Grade): Checks if the 'Grade' column was part of the update.
+	The inserted and deleted tables: Special tables where inserted contains the new values and deleted contains the old values of the updated rows.
 
+
+	What is an After Update Trigger in T-SQL?
+	A procedural code that is automatically executed in response to an UPDATE event on a table.
+	
+	What are some common uses of After Update Triggers in T-SQL?
+	Logging changes
+	Updating related information
+	Enforcing constraints
+	
+	Which tables are used in an After Update Trigger?
+	Inserted
+	Deleted
+	
+	When is an After Update Trigger fired?
+	After an update operation
 
 */
