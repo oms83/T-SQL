23d88c41d71203b1f5f4833a902779ebd5c7fe11
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-31 21:37:36 +0300

    Update CTERecursiveTree.sql

--- CTERecursiveTree.sql
+++ CTERecursiveTree.sql
@@ -16,3 +16,15 @@ WITH EmployeeTreeHierarchy AS
 SELECT * FROM EmployeeTreeHierarchy
 select * from Employees7
 
+
+/*
+1	NULL	CEO	CEO	0
+2	1	VP of Sales	CEO -> VP of Sales	1
+3	1	VP of Marketing	CEO -> VP of Marketing	1
+9	1	VP IT	CEO -> VP IT	1
+6	3	Marketing Manager	CEO -> VP of Marketing -> Marketing Manager	2
+8	6	Marketing Specialist	CEO -> VP of Marketing -> Marketing Manager -> Marketing Specialist	3
+4	2	Sales Manager	CEO -> VP of Sales -> Sales Manager	2
+5	2	Sales Representative	CEO -> VP of Sales -> Sales Representative	2
+7	4	Sales Associate	CEO -> VP of Sales -> Sales Manager -> Sales Associate	3
+*/
