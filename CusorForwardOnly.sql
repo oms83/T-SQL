2d0bc88d0a3d4ead39f7b2187c3dc0d572d24ad9
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-05-31 06:54:35 +0300

    Update CusorForwardOnly.sql

--- CusorForwardOnly.sql
+++ CusorForwardOnly.sql
@@ -1,21 +1,21 @@
-DECLARE CursorDynamic CURSOR FORWARD_ONLY FOR
+DECLARE ForwardOnly CURSOR FORWARD_ONLY FOR
 SELECT StudentID, Name, Grade FROM Students
 
 DECLARE @Name VARCHAR(100), @Grade INT, @StudentID INT;
 
-OPEN CursorDynamic;
+OPEN ForwardOnly;
 
-FETCH NEXT FROM CursorDynamic INTO @StudentID, @Name, @Grade;
+FETCH NEXT FROM ForwardOnly INTO @StudentID, @Name, @Grade;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN 
 	
 	PRINT 'Student Name: ' + @Name + '  Grade: ' + CAST(@Grade AS VARCHAR); 
 	
-	FETCH NEXT FROM CursorDynamic INTO @StudentID, @Name, @Grade;
+	FETCH NEXT FROM ForwardOnly INTO @StudentID, @Name, @Grade;
 
 END
 
-CLOSE CursorDynamic;
+CLOSE ForwardOnly;
 
-DEALLOCATE CursorDynamic;
+DEALLOCATE ForwardOnly;
