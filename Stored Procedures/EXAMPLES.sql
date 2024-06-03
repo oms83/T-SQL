c5726df345c5938ff9d78477b86d334ac7bac052
Ömer Memes <109181440+oms83@users.noreply.github.com>
2024-06-03 10:53:40 +0300

    Update EXAMPLES.sql

--- Stored Procedures/EXAMPLES.sql
+++ Stored Procedures/EXAMPLES.sql
@@ -95,3 +95,87 @@ person info updated successfully
 */
 --------------------------------------------
 
+
+-- Find By ID 1
+create procedure SP_GetPersonInfoByID1
+	@personID int
+as 
+begin 
+	select * from People where PersonID = @personID
+end
+
+exec SP_GetPersonInfoByID1
+	@personID =1
+/*
+1	OMER	MEMES	oms83@gmail.com
+*/
+--------------------------------------------
+
+
+-- Find By ID 2
+create procedure SP_GetPersonInfoByID2
+	@personID int,
+	@firstName varchar(100) out,
+	@lastName varchar(100) output,
+	@email varchar(100) out,
+	@isFound bit out
+as 
+begin
+	
+	--if (select 1 from People where PersonID = @personID) = 1
+	if EXISTS(select 1 from People where PersonID = @personID)
+	begin 
+		select @firstName = FirstName, @lastName = LastName , @email = Email 
+		from People
+		where PersonID = @personID;
+
+		set @isFound = 1;
+	end
+	else	
+		set @isFound = 0;
+end
+
+declare @personID int = 1;
+declare @firstName varchar(100);
+declare @lastName varchar(100);
+declare @email varchar(100);
+declare @isFound bit;
+
+exec SP_GetPersonInfoByID2
+	@personID = @personID,
+	@firstName = @firstName out,
+	@lastName = @lastName out,
+	@email = @email out,
+	@isFound = @isFound out
+
+if @isFound = 1
+	select @personID 'Person ID', @firstName as 'First Name', @lastName as 'Last Name', @email as Email
+else
+	select 'Person with ID: ' + cast (@personID as varchar) + ' is not found'
+--------------------------------------------
+
+
+-- IsExists
+create procedure SP_IsExistByID
+	@personID int
+as
+begin
+	if EXISTS(select * from People where PersonID = @personID)
+		return 1
+	else	
+		return 0
+end
+
+
+declare @isExists bit;
+declare  @ID int = 1;
+execute @isExists = SP_IsExistByID
+	@personID = @ID
+
+if @isExists = 1
+	select 'Person with ID: ' + cast (@ID as varchar) + ' is found';
+else	
+	select 'Person with ID: ' + cast (@ID as varchar) + ' is not found'
+	
+--------------------------------------------
+
